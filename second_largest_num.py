f8e3993742ec306ae3135994aa5af00f3b874e4e
doney25 <doneysiby2026@cs.sjcetpalai.ac.in>
2025-05-18 21:23:01 +0530

    new

---
+++ second_largest_num.py
@@ -0,0 +1,16 @@
+def sec_lnum(arr):
+   
+    if len(arr) < 2:
+        return None
+
+    first = second = float('-inf')
+
+    for num in arr:
+        if num > first:
+            second = first
+            first = num
+        elif first > num > second:
+            second = num
+
+    return second
+print(sec_lnum([1, 2, 3, 4, 5]))
